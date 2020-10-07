1bde7c97b23f9eb3e5c079ef668183e3e32fdd35
odinokun <odinokun@gmail.com>
2020-10-07 14:52:57 +0300

    did doctor page

--- gulp/tasks/copy.js.js
+++ gulp/tasks/copy.js.js
@@ -1,9 +1,12 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function () {
 
-  $.gulp.task('copy:js', function() {
-    return $.gulp.src('src/js/uncompressed.js')
+  $.gulp.task('copy:js', function () {
+    return $.gulp.src([
+      'src/js/uncompressed.js',
+      'src/js/footer-map.js',
+      'src/js/doctor-map.js'])
       .pipe($.gulp.dest($.config.root + '/assets/js'))
   })
 
